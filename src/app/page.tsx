33b8c5d9f9b963389e8fe1093dc751d919e04a2c
Anshuman <78845166+CodingBoyBlah@users.noreply.github.com>
2025-06-30 18:49:43 +0530

    Added links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import icon from "./icon-128.png";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { GitHubLogo, XLogo, BlueSkyLogo, RedditLogo, DiscordLogo } from "@/components/ui/link-logo";
 
 export default function Home() {
   return (
@@ -20,6 +21,27 @@ export default function Home() {
         <Button asChild variant="outline" className="mt-10 scale-130">
           <Link href="https://github.com/solbrowse/extension">Download</Link>
         </Button>
+
+      { /* fill with #2e2e2e */}
+        <div className="flex items-center justify-center mt-10 space-x-4">
+          <Link href="https://github.com/solbrowse">
+            <GitHubLogo />
+          </Link>
+          <Link href="https://x.com/solbrowse">
+            <XLogo />
+          </Link>
+          <Link href="https://bsky.app/profile/solbrowse.bsky.social">
+            <BlueSkyLogo />
+          </Link>
+          <Link href="https://bsky.app/profile/solbrowse.bsky.social">
+            <RedditLogo />
+          </Link>
+          <Link href="https://discord.com/invite/4qQzJCbagq">
+            <DiscordLogo />
+          </Link>
+          
+
+        </div>
       </div>
 
       {/* Video Section: scrolls below ok */}
